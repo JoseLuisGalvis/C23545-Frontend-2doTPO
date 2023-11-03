e251138fa7eb001951b06c059c16235e043bed94
giselleZe <zelarangiselle@gmail.com>
2023-11-03 19:36:06 -0300

    se agregan validaciones de campos

--- tickets/index.js
+++ tickets/index.js
@@ -6,6 +6,9 @@ const countSummary = document.querySelector('#count-summary');
 const category = document.querySelector('#category');
 const btnClear = document.querySelector('#btn-clear');
 const btnSend = document.querySelector('#btn-send');
+const form = document.querySelector('#form_resumen');
+
+
 
 const valueTicket = 200;
 const percentageDiscount = {
@@ -17,16 +20,53 @@ const percentageDiscount = {
 const enableBtnSend = () => {
     const inputs = Array.from(document.querySelectorAll('input'));
     const allInputsHaveValue = inputs.every(input => input.value.length >= 1);
-
+    console.log(inputs);
     allInputsHaveValue
         ? btnSend.removeAttribute('disabled')
         : btnSend.setAttribute('disabled', true);
 }
 
-const handleChange = () => {
+const handleChange = (parm) => {
+    parm.preventDefault();
     const isDisableBtnClear = btnClear.hasAttribute('disabled');
     if(isDisableBtnClear) btnClear.removeAttribute('disabled');
-
+    //agrego control formato email
+    if (parm.target.id==='email'){   
+        const validEmail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+	
+	    if ( !validEmail.test(parm.target.value) ){
+		    alert('El Email ingresado es inválido');
+                
+            email.value='';            
+            email.focus();
+            enableBtnSend();
+		    return ;
+	    }
+    }
+    //valido campo cantidad
+    if (parm.target.id==='count'){    
+        //evaluo q sea numero ()
+        if (isNaN(parm.target.value)||parm.target.value.length==0||!Number.isInteger(parseFloat(parm.target.value))) {            
+            alert('La cantidad debe ser un numero entero entre 1 y 10');            
+            count.value='1';            
+            count.focus();
+            enableBtnSend();
+		    return ;
+        }
+	    if ( parseInt(parm.target.value)<=0 || parseInt(parm.target.value)>10){
+            if (parseInt(parm.target.value)<=0){
+		        alert('La cantidad debe ser mínimo 1');
+                count.value='1';            
+            }
+            if (parseInt(parm.target.value)>10){
+		       alert('La cantidad debe ser como máximo 10');
+                count.value='10';            
+            }
+            count.focus();            
+            enableBtnSend();
+		    return ;
+	    }
+    }
     enableBtnSend();
 }
 
@@ -47,9 +87,10 @@ const getSummary = (event) => {
     const totalDiscount = (valueTicket * percentageDiscount[category.value]) / 100;
     const totalValue = count.value * valueTicket;
     const fianlValue = totalValue -totalDiscount;
-    
+    alert(`Felicidades ${nameUser.value.toUpperCase() } ${lastName.value.toUpperCase()}, está por adquirir ${count.value} tickets categoria ${category.options[category.selectedIndex].text.toUpperCase()}`);
     const alertSummary = `
-        <p class="alert alert-primary w-100">Total a pagar: $ ${fianlValue}</p>
+        <p class="alert alert-success w-100">Total a pagar: $ ${fianlValue} 
+        </p>
     `;
 
     countSummary.innerHTML = alertSummary;
@@ -58,8 +99,11 @@ const getSummary = (event) => {
 nameUser.addEventListener('change', handleChange);
 lastName.addEventListener('change', handleChange);
 email.addEventListener('change', handleChange);
+//count.addEventListener('change', handleChange,count.id);
 count.addEventListener('change', handleChange);
 category.addEventListener('change', handleChange);
 
 btnClear.addEventListener('click', clearForm);
 btnSend.addEventListener('click', getSummary)
+form.addEventListener('submit', function() { return false; })
+//form.addEventListener.submit(function() { return false; });
